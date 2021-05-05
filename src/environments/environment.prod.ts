3fc0172706f7b0c4b4e04f50db546566eb6db8d3
Tiago Henrique Iwamoto <tiago.iwamoto@gmail.com>
2021-05-04 22:14:52 -0300

    Ajuste de url para primeiro release

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  // url: 'https://caca-casa-microservice.herokuapp.com',
-  url: 'http://localhost:8082',
+  url: 'https://caca-casa-microservice.herokuapp.com',
+  // url: 'http://localhost:8082',
   production: true
 };
